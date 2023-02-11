aadbbb39f210397a5e952a2dae4d7afc86a425f0
scottyp89 <scott@scottprudence.com>
2023-02-11 10:07:04 +0000

    Another commit

--- html/index.php
+++ html/index.php
@@ -40,55 +40,57 @@
                 </div>
             </div>
         </div>
-        <div class="table-responsive-sm">
-            <?php $data = GetData();?>
-            <table class="table table-striped table-dark table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Systolic first</th>
-                        <th scope="col">Diastolic first</th>
-                        <th scope="col">Systolic second</th>
-                        <th scope="col">Diastolic second</th>
-                        <th scope="col">Pulse</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if ($data->num_rows > 0) {
-                        while($row = $data->fetch_assoc()) {?>
-                            <div class="modal fade" id="DeleteEntryModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content bg-dark text-light">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Delete entry</h1>
-                                            <button type="button" class="btn-close text-light" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <div class="mb-3">
-                                                <p>Are you sure you want to delete this entry?</p>
+        <div class="container">
+            <div class="table-responsive-sm">
+                <?php $data = GetData();?>
+                <table class="table table-striped table-dark table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Date</th>
+                            <th scope="col">Systolic first</th>
+                            <th scope="col">Diastolic first</th>
+                            <th scope="col">Systolic second</th>
+                            <th scope="col">Diastolic second</th>
+                            <th scope="col">Pulse</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if ($data->num_rows > 0) {
+                            while($row = $data->fetch_assoc()) {?>
+                                <div class="modal fade" id="DeleteEntryModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content bg-dark text-light">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Delete entry</h1>
+                                                <button type="button" class="btn-close text-light" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                            <a href="delete.php?id=<?php echo $row["id"]; ?>"><button type="button" class="btn btn-danger">Delete</button></a>
+                                            <div class="modal-body">
+                                                <div class="mb-3">
+                                                    <p>Are you sure you want to delete this entry?</p>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <a href="delete.php?id=<?php echo $row["id"]; ?>"><button type="button" class="btn btn-danger">Delete</button></a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <tr>
-                                <td><?php echo $row["datetime"];?></td>
-                                <td><?php echo $row["systolic_1"];?></td>
-                                <td><?php echo $row["diastolic_1"];?></td>
-                                <td><?php echo $row["systolic_2"];?></td>
-                                <td><?php echo $row["diastolic_2"];?></td>
-                                <td><?php echo $row["pulse"];?></td>
-                                <td><a class="text-danger" href="#" data-bs-toggle="modal" data-bs-target="#DeleteEntryModal"><i class="fa-solid fa-trash-can"></i></a></td>
-                            </tr>   
-                        <?php }
-                    } ?>
-                </tbody>
-            </table>
+                                <tr>
+                                    <td><?php echo $row["datetime"];?></td>
+                                    <td><?php echo $row["systolic_1"];?></td>
+                                    <td><?php echo $row["diastolic_1"];?></td>
+                                    <td><?php echo $row["systolic_2"];?></td>
+                                    <td><?php echo $row["diastolic_2"];?></td>
+                                    <td><?php echo $row["pulse"];?></td>
+                                    <td><a class="text-danger" href="#" data-bs-toggle="modal" data-bs-target="#DeleteEntryModal"><i class="fa-solid fa-trash-can"></i></a></td>
+                                </tr>   
+                            <?php }
+                        } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <script src="https://kit.fontawesome.com/2b0ac13106.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
